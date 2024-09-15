60e3a5f475c06c0dbcb1d852d8ff0051e7271631
fly_mc <me@flymc.cc>
2024-09-15 12:02:51 +0800

    fix

--- packages/backend/src/core/RoleService.ts
+++ packages/backend/src/core/RoleService.ts
@@ -35,11 +35,8 @@ export type RolePolicies = {
 	gtlAvailable: boolean;
 	ltlAvailable: boolean;
 	canPublicNote: boolean;
-<<<<<<< HEAD
-	mentionLimit: number;
-=======
 	canEditNote: boolean;
->>>>>>> 559cf1830 (feat(wip): note editing)
+	mentionLimit: number;
 	canInvite: boolean;
 	inviteLimit: number;
 	inviteLimitCycle: number;
@@ -68,11 +65,8 @@ export const DEFAULT_POLICIES: RolePolicies = {
 	gtlAvailable: true,
 	ltlAvailable: true,
 	canPublicNote: true,
-<<<<<<< HEAD
-	mentionLimit: 20,
-=======
 	canEditNote: true,
->>>>>>> 559cf1830 (feat(wip): note editing)
+	mentionLimit: 20,
 	canInvite: false,
 	inviteLimit: 0,
 	inviteLimitCycle: 60 * 24 * 7,
@@ -372,11 +366,8 @@ export class RoleService implements OnApplicationShutdown, OnModuleInit {
 			gtlAvailable: calc('gtlAvailable', vs => vs.some(v => v === true)),
 			ltlAvailable: calc('ltlAvailable', vs => vs.some(v => v === true)),
 			canPublicNote: calc('canPublicNote', vs => vs.some(v => v === true)),
-<<<<<<< HEAD
-			mentionLimit: calc('mentionLimit', vs => Math.max(...vs)),
-=======
 			canEditNote: calc('canEditNote', vs => vs.some(v => v === true)),
->>>>>>> 559cf1830 (feat(wip): note editing)
+			mentionLimit: calc('mentionLimit', vs => Math.max(...vs)),
 			canInvite: calc('canInvite', vs => vs.some(v => v === true)),
 			inviteLimit: calc('inviteLimit', vs => Math.max(...vs)),
 			inviteLimitCycle: calc('inviteLimitCycle', vs => Math.max(...vs)),
