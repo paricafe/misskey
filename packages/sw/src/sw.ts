98db77fa90b3a4edc45aaccbd1273f3f990b2715
fly_mc <me@flymc.cc>
2024-10-23 23:56:33 +0800

    restore sw.ts

--- packages/sw/src/sw.ts
+++ packages/sw/src/sw.ts
@@ -12,99 +12,56 @@ import { createEmptyNotification, createNotification } from '@/scripts/create-no
 import { swLang } from '@/scripts/lang.js';
 import * as swos from '@/scripts/operations.js';
 
-const CACHE_NAMES = {
-  assets: `assets-${_VERSION_}`,
-  emoji: `emoji-${_VERSION_}`,
-  twemoji: `twemoji-${_VERSION_}`,
-  fluentEmoji: `fluent-emoji-${_VERSION_}`,
-  vite: `vite-${_VERSION_}`,
-  locale: swLang.cacheName,
-};
-
-const CACHE_PATHS = {
-  [CACHE_NAMES.assets]: '/assets',
-  [CACHE_NAMES.emoji]: '/emoji',
-  [CACHE_NAMES.twemoji]: '/twemoji',
-  [CACHE_NAMES.fluentEmoji]: '/fluent-emoji',
-  [CACHE_NAMES.vite]: '/vite',
-};
-
-globalThis.addEventListener('install', (ev) => {
-  // ev.waitUntil(globalThis.skipWaiting());
+globalThis.addEventListener('install', () => {
+	// ev.waitUntil(globalThis.skipWaiting());
 });
 
 globalThis.addEventListener('activate', ev => {
-  ev.waitUntil(
-    caches.keys()
-      .then(cacheNames => Promise.all(
-        cacheNames
-          .filter((name) => {
-            return !Object.values(CACHE_NAMES).includes(name);
-          })
-          .map(name => caches.delete(name)),
-      ))
-      .then(() => globalThis.clients.claim()),
-  );
+	ev.waitUntil(
+		caches.keys()
+			.then(cacheNames => Promise.all(
+				cacheNames
+					.filter((v) => v !== swLang.cacheName)
+					.map(name => caches.delete(name)),
+			))
+			.then(() => globalThis.clients.claim()),
+	);
 });
 
 async function offlineContentHTML() {
-  const i18n = await (swLang.i18n ?? swLang.fetchLocale()) as Partial<I18n<Locale>>;
-  const messages = {
-    title: i18n.ts?._offlineScreen.title ?? 'Offline - Could not connect to server',
-    header: i18n.ts?._offlineScreen.header ?? 'Could not connect to server',
-    reload: i18n.ts?.reload ?? 'Reload',
-  };
+	const i18n = await (swLang.i18n ?? swLang.fetchLocale()) as Partial<I18n<Locale>>;
+	const messages = {
+		title: i18n.ts?._offlineScreen.title ?? 'Offline - Could not connect to server',
+		header: i18n.ts?._offlineScreen.header ?? 'Could not connect to server',
+		reload: i18n.ts?.reload ?? 'Reload',
+	};
 
   return `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta content="width=device-width,initial-scale=1"name="viewport"><title>${messages.title}</title><style>body{background-color:#0c1210;color:#dee7e4;font-family:Hiragino Maru Gothic Pro,BIZ UDGothic,Roboto,HelveticaNeue,Arial,sans-serif;line-height:1.35;display:flex;flex-direction:column;align-items:center;justify-content:center;min-height:100vh;margin:0;padding:24px;box-sizing:border-box}.icon{max-width:120px;width:100%;height:auto;margin-bottom:20px;}.message{text-align:center;font-size:20px;font-weight:700;margin-bottom:20px}.version{text-align:center;font-size:90%;margin-bottom:20px}button{padding:7px 14px;min-width:100px;font-weight:700;font-family:Hiragino Maru Gothic Pro,BIZ UDGothic,Roboto,HelveticaNeue,Arial,sans-serif;line-height:1.35;border-radius:99rem;background-color:#ff82ab;color:#192320;border:none;cursor:pointer;-webkit-tap-highlight-color:transparent}button:hover{background-color:#fac5eb}</style></head><body><svg class="icon"fill="none"height="24"stroke="currentColor"stroke-linecap="round"stroke-linejoin="round"stroke-width="2"viewBox="0 0 24 24"width="24"xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z"fill="none"stroke="none"/><path d="M9.58 5.548c.24 -.11 .492 -.207 .752 -.286c1.88 -.572 3.956 -.193 5.444 1c1.488 1.19 2.162 3.007 1.77 4.769h.99c1.913 0 3.464 1.56 3.464 3.486c0 .957 -.383 1.824 -1.003 2.454m-2.997 1.033h-11.343c-2.572 -.004 -4.657 -2.011 -4.657 -4.487c0 -2.475 2.085 -4.482 4.657 -4.482c.13 -.582 .37 -1.128 .7 -1.62"/><path d="M3 3l18 18"/></svg><div class="message">${messages.header}</div><div class="version">v${_VERSION_}</div><button onclick="reloadPage()">${messages.reload}</button><script>function reloadPage(){location.reload(!0)}</script></body></html>`;
 }
 
 globalThis.addEventListener('fetch', ev => {
-  const url = new URL(ev.request.url);
-  const matchedCache = Object.entries(CACHE_PATHS).find(([, path]) => url.pathname.startsWith(path));
+	let isHTMLRequest = false;
+	if (ev.request.headers.get('sec-fetch-dest') === 'document') {
+		isHTMLRequest = true;
+	} else if (ev.request.headers.get('accept')?.includes('/html')) {
+		isHTMLRequest = true;
+	} else if (ev.request.url.endsWith('/')) {
+		isHTMLRequest = true;
+	}
 
-  if (matchedCache) {
-    ev.respondWith(
-      caches.open(matchedCache[0])
-        .then(cache => 
-          cache.match(ev.request)
-            .then(response => {
-              if (response) {
-                return response;
-              }
-              return fetch(ev.request)
-                .then(response => {
-                  cache.put(ev.request, response.clone());
-                  return response;
-                });
-            })
-        )
-        .catch(() => fetch(ev.request))
-    );
-    return;
-  }
-
-  let isHTMLRequest = false;
-  if (ev.request.headers.get('sec-fetch-dest') === 'document') {
-    isHTMLRequest = true;
-  } else if (ev.request.headers.get('accept')?.includes('/html')) {
-    isHTMLRequest = true;
-  } else if (ev.request.url.endsWith('/')) {
-    isHTMLRequest = true;
-  }
-
-  if (!isHTMLRequest) return;
-  ev.respondWith(
-    fetch(ev.request)
-      .catch(async () => {
-        const html = await offlineContentHTML();
-        return new Response(html, {
-          status: 200,
-          headers: {
-            'content-type': 'text/html',
-          },
-        });
-      }),
-  );
+	if (!isHTMLRequest) return;
+	ev.respondWith(
+		fetch(ev.request)
+			.catch(async () => {
+				const html = await offlineContentHTML();
+				return new Response(html, {
+					status: 200,
+					headers: {
+						'content-type': 'text/html',
+					},
+				});
+			}),
+	);
 });
 
 globalThis.addEventListener('push', ev => {
