8120e320ab4fff09980d5073ed04dce060dac93b
fly_mc <me@flymc.cc>
2024-11-02 23:37:22 +0800

    detect-language fallback to local

--- packages/frontend/src/scripts/detect-language.ts
+++ packages/frontend/src/scripts/detect-language.ts
@@ -5,14 +5,16 @@
 
 import { detect } from 'tinyld/heavy';
 import * as mfm from 'mfm-js';
+import { miLocalStorage } from '@/local-storage.js';
 
 export default function detectLanguage(text: string): string {
+	const localLang = (miLocalStorage.getItem('lang') ?? navigator.language).slice(0, 2);
 	const nodes = mfm.parse(text);
 	const filtered = mfm.extract(nodes, (node) => {
 		return node.type === 'text' || node.type === 'quote';
 	});
 	const purified = mfm.toString(filtered);
 
-	if (detect(purified) === '') return 'en';
+	if (detect(purified) === '') return localLang;
 	return detect(purified);
 }
