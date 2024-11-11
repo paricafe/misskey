edd83acaab9d62ad7098edde6f0c2d5e76ad00b9
fly_mc <me@flymc.cc>
2024-11-11 21:46:05 +0800

    fix cw text pgroonga

--- packages/backend/src/models/Note.ts
+++ packages/backend/src/models/Note.ts
@@ -75,9 +75,9 @@ export class MiNote {
 	})
 	public name: string | null;
 
-	@Index() // USING pgroonga pgroonga_varchar_full_text_search_ops_v2
-	@Column('varchar', {
-		length: 512, nullable: true,
+	@Index() // USING pgroonga
+	@Column('text', {
+		nullable: true,
 	})
 	public cw: string | null;
 
