1471039199254794edf101bc3ee5d6f6d73c126a
fly_mc <me@flymc.cc>
2024-11-18 02:41:44 +0800

    Revert "update SearchService"
    This reverts commit a9acf532c123ac16e02b47dbded22ca0fb2dfa6a.

--- packages/backend/src/core/SearchService.ts
+++ packages/backend/src/core/SearchService.ts
@@ -4,7 +4,7 @@
  */
 
 import { Inject, Injectable } from '@nestjs/common';
-import { Brackets, In } from 'typeorm';
+import { In } from 'typeorm';
 import { DI } from '@/di-symbols.js';
 import type { Config } from '@/config.js';
 import { bindThis } from '@/decorators.js';
@@ -215,10 +215,7 @@ export class SearchService {
 			}
 
 			query
-				.andWhere(new Brackets((qb => {
-					qb.where('note.text &@~ :q', { q: sqlLikeEscape(q) })
-						.orWhere('note.cw &@~ :q', { q: sqlLikeEscape(q) });
-				})))
+				.andWhere('note.text &@~ :q', { q: sqlLikeEscape(q) })
 				.innerJoinAndSelect('note.user', 'user')
 				.leftJoinAndSelect('note.reply', 'reply')
 				.leftJoinAndSelect('note.renote', 'renote')
