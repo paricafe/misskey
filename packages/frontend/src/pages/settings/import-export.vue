4898087ddb02533acccf675b0a46f694f8e079c4
fly_mc <me@flymc.cc>
2024-11-19 02:21:20 +0800

    frontend: fix import note icon

--- packages/frontend/src/pages/settings/import-export.vue
+++ packages/frontend/src/pages/settings/import-export.vue
@@ -39,7 +39,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<option value="Instagram">Instagram</option>
 					<option value="Facebook">Facebook</option>
 				</MkRadios>
-				<MkButton primary :class="$style.button" inline @click="importNotes($event)"><i class="ph-upload ph-bold ph-lg"></i> {{ i18n.ts.import }}</MkButton>
+				<MkButton primary :class="$style.button" inline @click="importNotes($event)"><i class="ti ti-upload"></i> {{ i18n.ts.import }}</MkButton>
 			</MkFolder>
 		</div>
 	</FormSection>
