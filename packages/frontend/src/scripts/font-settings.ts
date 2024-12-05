436e306cbc8178bcd9e967a22d403252a8695d1d
Lhc_fl <lhcfl@outlook.com>
2024-12-06 01:42:05 +0800

    fix font importer

--- packages/frontend/src/scripts/font-settings.ts
+++ packages/frontend/src/scripts/font-settings.ts
@@ -1,4 +1,5 @@
 import { computed, ref, watch } from 'vue';
+import { loadFontStyle } from './load-font.js';
 import type { Ref } from 'vue';
 import { miLocalStorage } from '@/local-storage.js';
 import { i18n } from '@/i18n.js';
@@ -52,14 +53,6 @@ function getFontId(name: string, option: string) {
 	}
 }
 
-async function loadFontStyle(fontId: string) {
-	try {
-		await import(`@/styles-font/${fontId}.scss`);
-	} catch (e) {
-		console.warn(`Failed to load font style: ${fontId}`, e);
-	}
-}
-
 export function getDefaultFontSettings() {
 	const def_arr = miLocalStorage.getItem('defaultFontFace')?.split('_');
 	const fontFace = ref(def_arr?.[0] ?? 'system-ui');
@@ -76,10 +69,7 @@ export function getDefaultFontSettings() {
 		miLocalStorage.setItem('defaultFontFace', newFontId);
 		document.documentElement.classList.add(`default-font-${newFontId}`);
 
-		if (fontFace.value !== 'system-ui') {
-			await loadFontStyle(fontFace.value);
-		}
-		console.log(newFontId);
+		await loadFontStyle(fontFace.value);
 	}
 
 	watch(fontFace, (newVal) => {
---
+++ packages/frontend/src/scripts/load-font.ts
@@ -0,0 +1,16 @@
+const defaultFontsList = [
+	'roboto',
+	'misskey-biz',
+	'arial',
+	'times',
+	'system-ui',
+];
+
+export async function loadFontStyle(fontId: string) {
+	if (defaultFontsList.includes(fontId)) return;
+	try {
+		await import(`@/styles-font/${fontId}.scss`);
+	} catch (err) {
+		console.warn(`Failed to load font style: ${fontId}`, err);
+	}
+}
