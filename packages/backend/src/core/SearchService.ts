ad0589ffa0937a737da0d2234246a7f65109c891
FLY_MC <me@flymc.cc>
2025-01-24 14:20:57 +0800

    Revert "backend: add cw sqlLike search"
    This reverts commit 625a7621582db5d779da86e1e61cbf8bf78f09e3.

--- packages/backend/src/core/SearchService.ts
+++ packages/backend/src/core/SearchService.ts
@@ -4,7 +4,7 @@
  */
 
 import { Inject, Injectable } from '@nestjs/common';
-import { Brackets, In } from 'typeorm';
+import { In, SelectQueryBuilder } from 'typeorm';
 import { DI } from '@/di-symbols.js';
 import { type Config, FulltextSearchProvider } from '@/config.js';
 import { bindThis } from '@/decorators.js';
@@ -220,15 +220,9 @@ export class SearchService {
 			.leftJoinAndSelect('renote.user', 'renoteUser');
 
 		if (this.config.fulltextSearch?.provider === 'sqlPgroonga') {
-			query.andWhere(new Brackets(qb => {
-				qb.where('note.text &@ :q', { q })
-				  .orWhere('note.cw &@ :q', { q });
-			}));
+			query.andWhere('note.text &@ :q', { q });
 		} else {
-			query.andWhere(new Brackets(qb => {
-				qb.where('LOWER(note.text) LIKE :q', { q: `%${sqlLikeEscape(q.toLowerCase())}%` })
-				  .orWhere('LOWER(note.cw) LIKE :q', { q: `%${sqlLikeEscape(q.toLowerCase())}%` });
-			}));
+			query.andWhere('LOWER(note.text) LIKE :q', { q: `%${ sqlLikeEscape(q.toLowerCase()) }%` });
 		}
 
 		if (opts.host) {
