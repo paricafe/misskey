e31e1dbec95e7d1467dbeee38749d1bd2374107b
FLY_MC <me@flymc.cc>
2025-07-03 01:16:12 +0800

    fix: admin approve page (Pagination refactor)

--- packages/frontend/src/pages/admin/approvals.vue
+++ packages/frontend/src/pages/admin/approvals.vue
@@ -8,7 +8,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 	<PageWithHeader :actions="headerActions" :tabs="headerTabs">
 		<div class="_spacer" style="--MI_SPACER-w: 900px;">
 			<div class="_gaps_m">
-				<MkPagination ref="paginationComponent" :pagination="pagination" :displayLimit="50">
+				<MkPagination :paginator="paginator" :displayLimit="50">
 					<template #default="{ items }">
 						<div class="_gaps_s">
 							<MkApprovalUser v-for="item in items" :key="item.id" :user="(item as any)" :onDeleted="deleted"/>
@@ -22,29 +22,25 @@ SPDX-License-Identifier: AGPL-3.0-only
 </template>
 
 <script lang="ts" setup>
-import { computed, useTemplateRef } from 'vue';
+import { computed, useTemplateRef, ref, markRaw } from 'vue';
 import MkPagination from '@/components/MkPagination.vue';
 import MkApprovalUser from '@/components/MkApprovalUser.vue';
+import { Paginator } from '@/utility/paginator.js';
 import { i18n } from '@/i18n.js';
 import { definePage } from '@/page.js';
 
-let paginationComponent = useTemplateRef<InstanceType<typeof MkPagination>>('paginationComponent');
-
-const pagination = {
-	endpoint: 'admin/show-users' as const,
+const paginator = markRaw(new Paginator('admin/show-users', {
 	limit: 10,
-	params: computed(() => ({
+	computedParams: computed(() => ({
 		sort: '+createdAt',
 		state: 'approved',
 		origin: 'local',
 	})),
 	offsetMode: true,
-};
+}));
 
 function deleted(id: string) {
-	if (paginationComponent.value) {
-		paginationComponent.value.items.delete(id);
-	}
+	paginator.removeItem(id);
 }
 
 const headerActions = computed(() => []);
