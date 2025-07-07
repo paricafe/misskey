93f5e5c53943cff2316432630ef66499cde9b4f1
FLY_MC <me@flymc.cc>
2025-07-07 12:25:58 +0800

    make custom emoji in remote notes reactable

--- packages/frontend/src/components/global/MkCustomEmoji.vue
+++ packages/frontend/src/components/global/MkCustomEmoji.vue
@@ -122,7 +122,7 @@ function onClick(ev: MouseEvent) {
 			});
 		}
 
-		if (props.menuReaction && react) {
+		if ((props.menuReaction || customEmojisMap.has(customEmojiName.value)) && react) { //if local has custom emoji, allow reaction
 			menuItems.push({
 				text: i18n.ts.doReaction,
 				icon: 'ti ti-plus',
