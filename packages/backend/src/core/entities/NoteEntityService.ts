7996ff5ad9e11ec1e8f4c6fae2d529efafddbc6b
Natsuki-Kaede <i@pari.network>
2026-05-02 18:53:19 +0800

    fix

--- packages/backend/src/core/entities/NoteEntityService.ts
+++ packages/backend/src/core/entities/NoteEntityService.ts
@@ -19,7 +19,6 @@ import { shouldHideNoteByTime } from '@/misc/should-hide-note-by-time.js';
 import { ReactionsBufferingService } from '@/core/ReactionsBufferingService.js';
 import { CacheService } from '@/core/CacheService.js';
 import type { OnModuleInit } from '@nestjs/common';
-import type { CacheService } from '../CacheService.js';
 import type { CustomEmojiService } from '../CustomEmojiService.js';
 import type { ReactionService } from '../ReactionService.js';
 import type { UserEntityService } from './UserEntityService.js';
