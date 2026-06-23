8456c27146f465130fa8c987e04e4b6ebafc9f95
Natsuki-Kaede <i@pari.network>
2026-06-24 01:09:09 +0800

    fix

--- packages/backend/src/core/activitypub/type.ts
+++ packages/backend/src/core/activitypub/type.ts
@@ -72,6 +72,18 @@ export function getApId(value: string | IObject | undefined): string {
 }
 
 /**
+ * Get ActivityStreams Object id, or null if not present
+ */
+export function getNullableApId(value: string | IObject | [string | IObject]): string | null {
+	// eslint-disable-next-line no-param-reassign
+	value = fromTuple(value);
+
+	if (typeof value === 'string') return value;
+	if (typeof value.id === 'string') return value.id;
+	return null;
+}
+
+/**
  * Get ActivityStreams Object type
  *
  * タイプ判定ができなかった場合に、あえてエラーではなくnullを返すようにしている。
